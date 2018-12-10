888c0ecb4799a1c24a8c53f05ad8cb5abfe731d3
Quinton Lee <quivistis@gmail.com>
2018-12-09 21:32:31 -0600

    Tools: add sampling and shuffle methods

--- src/tools.ts
+++ src/tools.ts
@@ -1,4 +1,48 @@
 export class Tools {
+	random(limit?: number) {
+		if (!limit) limit = 2;
+		return Math.floor(Math.random() * limit);
+	}
+
+	sampleMany<T>(array: T[], amount: string | number): T[] {
+		const len = array.length;
+		if (!len) throw new Error("Tools.sampleMany() does not accept empty arrays");
+		if (len === 1) return array.slice();
+		if (typeof amount === 'string') amount = parseInt(amount);
+		if (!amount || isNaN(amount)) throw new Error("Invalid amount in Tools.sampleMany()");
+		if (amount > len) amount = len;
+		return this.shuffle(array).splice(0, amount);
+	}
+
+	sampleOne<T>(array: T[]): T {
+		const len = array.length;
+		if (!len) throw new Error("Tools.sampleOne() does not accept empty arrays");
+		if (len === 1) return array.slice()[0];
+		return this.shuffle(array)[0];
+	}
+
+	shuffle<T>(array: T[]): T[] {
+		array = array.slice();
+
+		// Fisher-Yates shuffle algorithm
+		let currentIndex = array.length;
+		let randomIndex = 0;
+		let temporaryValue;
+
+		// While there remain elements to shuffle...
+		while (currentIndex !== 0) {
+			// Pick a remaining element...
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+
+			// And swap it with the current element.
+			temporaryValue = array[currentIndex];
+			array[currentIndex] = array[randomIndex];
+			array[randomIndex] = temporaryValue;
+		}
+		return array;
+	}
+
 	toId(input: string | number | {id: string}): string {
 		if (typeof input !== 'string') {
 			if (typeof input === 'number') {
