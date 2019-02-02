b41e570ebe1541158d34623b9b0f9fbe6335bc0e
Quinton Lee <quivistis@gmail.com>
2019-02-02 12:58:03 -0600

    Fix setting up Pokemon-Showdown folder on Travis CI

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
-
-const PokemonShowdown = path.join(__dirname, 'Pokemon-Showdown');
-if (!fs.existsSync(PokemonShowdown)) {
-	console.log("Setting up Pokemon-Showdown folder...");
-	child_process.execSync('git clone https://github.com/Zarel/Pokemon-Showdown.git');
-} else {
-	console.log("Updating Pokemon-Showdown files...");
-	process.chdir(PokemonShowdown);
-	child_process.execSync('git pull');
-	process.chdir(__dirname);
-}
 
 const configFile = path.join(__dirname, 'src', 'config.ts');
 if (!fs.existsSync(configFile)) {
--- build.js
+++ build.js
@@ -33,6 +33,17 @@ module.exports = async (resolve, reject) => {
 	deleteFolderRecursive(builtFolder);
 
 	(async () => {
+		const PokemonShowdown = path.join(__dirname, 'Pokemon-Showdown');
+		if (!fs.existsSync(PokemonShowdown)) {
+			console.log("Setting up Pokemon-Showdown folder...");
+			child_process.execSync('git clone https://github.com/Zarel/Pokemon-Showdown.git');
+		} else {
+			console.log("Updating Pokemon-Showdown files...");
+			process.chdir(PokemonShowdown);
+			child_process.execSync('git pull');
+			process.chdir(__dirname);
+		}
+
 		console.log("Running tsc...");
 		const exec = util.promisify(child_process.exec);
 		const build = await exec('npm run tsc', {stdio: 'inherit'}).catch(e => console.log(e));
