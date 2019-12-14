699bf5bdb30ec667e1cc3053f3cf8cf344fa5853
Quinton Lee <quivistis@gmail.com>
2019-12-13 22:33:12 -0600

    Add Crustle's Crumbling Blocks

---
+++ src/games/crustles-crumbling-blocks.ts
@@ -0,0 +1,148 @@
+import { ICommandDefinition } from "../command-parser";
+import { Player } from "../room-activity";
+import { Game } from "../room-game";
+import { IGameFile } from "../types/games";
+
+const MIN_BLOCKS = 1;
+const MAX_BLOCKS = 5;
+
+class CrustlesCrumblingBlocks extends Game {
+	currentPlayer: Player | null = null;
+	blocks: number = 0;
+	order: Player[] = [];
+	playerList: Player[] = [];
+
+	onRemovePlayer() {
+		if (!this.started) return;
+		this.currentPlayer = null;
+		this.nextRound();
+	}
+
+	onStart() {
+		this.nextRound();
+	}
+
+	onNextRound() {
+		if (this.currentPlayer) {
+			this.say("**" + this.currentPlayer.name + "** did not remove any blocks and has been eliminated from the game! The blocks will now reset.");
+			this.eliminatePlayer(this.currentPlayer, "You did not remove any blocks!");
+			this.currentPlayer = null;
+		}
+
+		const remainingPlayerCount = this.getRemainingPlayerCount();
+		if (remainingPlayerCount === 2) {
+			this.say("There are no blocks remaining!");
+			this.timeout = setTimeout(() => this.end(), 5 * 1000);
+			return;
+		} else if (remainingPlayerCount <= 1) {
+			this.say("The game has ended due to a lack of players.");
+			this.end();
+			return;
+		}
+
+		this.order = this.shufflePlayers(this.getRemainingPlayers());
+		this.playerList = this.order.slice();
+		this.currentPlayer = this.playerList[0];
+		this.playerList.shift();
+
+		const html = this.getRoundHtml(this.getPlayerNames);
+		const uhtmlName = this.uhtmlBaseName + '-round-html';
+		this.onUhtml(uhtmlName, html, () => {
+			this.timeout = setTimeout(() => {
+				let blocks = remainingPlayerCount * 3;
+				if (blocks > 25) {
+					blocks = 25;
+				} else if (blocks < 10) {
+					blocks = 10;
+				}
+				this.blocks = blocks;
+				this.chooseNextPlayer();
+			}, 5000);
+		});
+		this.sayUhtml(uhtmlName, html);
+	}
+
+	chooseNextPlayer() {
+		if (!this.getRemainingPlayerCount()) {
+			this.nextRound();
+			return;
+		}
+
+		this.currentPlayer = null;
+		if (!this.playerList.length) this.playerList = this.order.slice();
+		let currentPlayer = this.playerList[0];
+		this.playerList.shift();
+		while (currentPlayer && currentPlayer.eliminated) {
+			if (!this.playerList.length) this.playerList = this.order.slice();
+			currentPlayer = this.playerList[0];
+			this.playerList.shift();
+		}
+		if (!currentPlayer || currentPlayer.eliminated) {
+			this.nextRound();
+			return;
+		}
+		if (this.blocks - MIN_BLOCKS <= 0) {
+			this.removeLastBlock(currentPlayer);
+			return;
+		}
+
+		const text = "**" + currentPlayer.name + "**, you are up! There " + (this.blocks > 1 ? "are" : "is") + " currently **" + this.blocks + "** block" + (this.blocks > 1 ? "s" : "") + " remaining.";
+		this.on(text, () => {
+			this.currentPlayer = currentPlayer;
+			this.timeout = setTimeout(() => this.nextRound(), 30 * 1000);
+		});
+		this.say(text);
+	}
+
+	removeLastBlock(player: Player) {
+		this.say("**" + player.name + "** was forced to remove the last block from the pyramid and has been eliminated from the game!");
+		this.eliminatePlayer(player, "You removed the last block from the pyramid!");
+		this.currentPlayer = null;
+		this.timeout = setTimeout(() => this.nextRound(), 5 * 1000);
+	}
+
+	onEnd() {
+		const remainingPlayers = Object.keys(this.getRemainingPlayers());
+		if (remainingPlayers.length) {
+			for (let i = 0; i < 2; i++) {
+				if (!remainingPlayers[i]) break;
+				this.winners.set(this.players[remainingPlayers[i]], 1);
+				this.addBits(this.players[remainingPlayers[i]], i === 0 ? 500 : 250);
+			}
+		}
+		this.announceWinners();
+	}
+}
+
+const commands: Dict<ICommandDefinition<CrustlesCrumblingBlocks>> = {
+	remove: {
+		command(target, room, user) {
+			if (!(user.id in this.players) || this.players[user.id].eliminated || this.players[user.id] !== this.currentPlayer) return false;
+			const player = this.players[user.id];
+			const targetNumber = parseInt(target);
+			if (isNaN(targetNumber) || targetNumber > MAX_BLOCKS || targetNumber < MIN_BLOCKS) {
+				this.say("You can only remove between " + MIN_BLOCKS + " and " + MAX_BLOCKS + " blocks at a time.");
+				return false;
+			}
+			if (this.timeout) clearTimeout(this.timeout);
+			this.blocks -= targetNumber;
+			if (this.blocks <= 0) {
+				this.removeLastBlock(player);
+				return false;
+			}
+			this.chooseNextPlayer();
+			return true;
+		},
+	},
+};
+
+export const game: IGameFile<CrustlesCrumblingBlocks> = {
+	aliases: ['crustles', 'ccb'],
+	class: CrustlesCrumblingBlocks,
+	commandDescriptions: [Config.commandCharacter + "remove [number of blocks]"],
+	commands,
+	description: "Players remove blocks from Crustle's pyramid until only one remains. The player forced to remove the final block is eliminated!",
+	name: "Crustle's Crumbling Blocks",
+	mascot: "Crustle",
+	scriptedOnly: true,
+};
