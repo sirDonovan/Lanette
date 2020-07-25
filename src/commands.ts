7fbd6b931b7a528d38ac4828097eec10f2385d00
Emboar02 <45805212+Emboar02@users.noreply.github.com>
2020-07-25 13:31:35 -0400

    Commands: add .gamedescription (#12)

--- src/commands.ts
+++ src/commands.ts
@@ -407,6 +407,39 @@ const commands: CommandDefinitions<CommandContext> = {
 		},
 		aliases: ['minigames'],
 	},
+	gamedescription: {
+		command(target, room, user) {
+			const targets = target.split(',');
+			let gameRoom: Room;
+			if (this.isPm(room)) {
+				const targetRoom = Rooms.search(Tools.toRoomId(targets[0]));
+				if (!targetRoom) return this.sayError(['invalidBotRoom', targets[0]]);
+				if (!user.rooms.has(targetRoom)) return this.sayError(['noPmHtmlRoom', targetRoom.title]);
+				gameRoom = targetRoom;
+				targets.shift();
+			} else {
+				if (!user.hasRank(room, 'voice') && !(room.userHostedGame && room.userHostedGame.isHost(user))) return;
+				gameRoom = room;
+			}
+			if (!Config.allowScriptedGames || !Config.allowScriptedGames.includes(gameRoom.id)) {
+				return this.sayError(['disabledGameFeatures', gameRoom.title]);
+			}
+			const format = Games.getFormat(targets[0], true);
+				if (Array.isArray(format)) {
+					const userHostedFormat = Games.getUserHostedFormat(targets[0]);
+					if (Array.isArray(userHostedFormat)) {
+						this.sayError(['invalidGameFormat', targets[0]]);
+						return;
+					}
+					const gameDesc = userHostedFormat.description;
+					this.say(userHostedFormat.name + ': ' + gameDesc);
+					return;
+				}
+				const gameDesc = format.description;
+				this.say(format.name + ': ' + gameDesc);
+		},
+		aliases: ['gamedesc', 'gdesc'],
+	},
 	startvote: {
 		command(target, room, user) {
 			if (this.isPm(room) || !user.hasRank(room, 'voice') || room.game || room.userHostedGame) return;
