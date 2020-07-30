3ce02beb0331d9ec542e4d9fd79caeb168438981
Quinton Lee <quivistis@gmail.com>
2020-07-30 09:07:51 -0500

    Add Catch and Evolve

---
+++ src/games/tournaments/catch-and-evolve.ts
@@ -0,0 +1,59 @@
+import { EliminationTournament, game as eliminationTournamentGame } from '../templates/elimination-tournament';
+import type { IGameFile } from '../../types/games';
+
+const name = "Catch and Evolve";
+const description = "Every player is given a randomly generated Pokemon to use as their starter. Each battle that you win, you " +
+	"may 'catch' 1 of your opponent's Pokemon (add it to your team) and then evolve 1 Pokemon on your team.";
+
+class CatchAndEvolve extends EliminationTournament {
+	additionsPerRound = 1;
+	evolutionsPerRound = 1;
+	startingTeamsLength = 1;
+	baseTournamentName = name;
+	tournamentDescription = description;
+	banlist = ['Burmy', 'Caterpie', 'Combee', 'Kricketot', 'Magikarp', 'Scatterbug', 'Sunkern', 'Tynamo', 'Type: Null', 'Weedle',
+		'Wurmple', 'Cosmog', 'Blipbug', 'Snom'];
+	tournamentRules = [
+		"- All moves, abilities, and items are allowed",
+		"- Mega evolutions and regional formes are allowed",
+		"- Scouting is NOT allowed",
+	];
+}
+
+export const game: IGameFile<CatchAndEvolve> = Games.copyTemplateProperties(eliminationTournamentGame, {
+	aliases: ['cande', 'ce', 'catchevolve'],
+	class: CatchAndEvolve,
+	description,
+	name,
+	variants: [
+		{
+			name: "Monotype Catch and Evolve",
+			variant: "monotype",
+		},
+		{
+			name: "Monoregion Catch and Evolve",
+			variant: "monoregion",
+			variantAliases: ["monogen"],
+		},
+		{
+			name: "Catch and Evolve Ubers",
+			variant: "ubers",
+		},
+		{
+			name: "Catch and Evolve UU",
+			variant: "uu",
+		},
+		{
+			name: "Catch and Evolve RU",
+			variant: "ru",
+		},
+		{
+			name: "Catch and Evolve NU",
+			variant: "nu",
+		},
+		{
+			name: "Catch and Evolve PU",
+			variant: "pu",
+		},
+	],
+});
