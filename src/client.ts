3c6967ccb8b87e6bae86666d472368fcc5d14214
Quinton Lee <quivistis@gmail.com>
2020-08-20 23:00:42 -0500

    Client: use Math.ceil for average latency

--- src/client.ts
+++ src/client.ts
@@ -235,7 +235,7 @@ export class Client {
 				for (const time of this.serverLatencyTimes) {
 					totalLatency += time;
 				}
-				this.averageServerLatency = totalLatency ? Math.floor(totalLatency / this.serverLatencyTimes.length) : 0;
+				this.averageServerLatency = totalLatency ? Math.ceil(totalLatency / this.serverLatencyTimes.length) : 0;
 				this.setStartServerPingsTimeout();
 			} else {
 				this.nextServerPing = setTimeout(() => this.pingServer(), 2 * 1000);
