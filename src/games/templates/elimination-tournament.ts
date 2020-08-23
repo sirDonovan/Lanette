585317ded71b427339598ac2a73ce9ddb1559ccb
Quinton Lee <quivistis@gmail.com>
2020-08-23 00:37:55 -0500

    Elimination tournament: increase test timeout

--- src/games/templates/elimination-tournament.ts
+++ src/games/templates/elimination-tournament.ts
@@ -1802,6 +1802,7 @@ const tests: GameFileTests<EliminationTournament> = {
 	},
 	'should give team changes until players have a full team - additionsPerRound >= 1': {
 		test(game, format) {
+			this.timeout(10000);
 			if (game.additionsPerRound < 1 || game.maxPlayers < 64) return;
 
 			addPlayers(game, 64);
