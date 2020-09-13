fdd4d69a4aad09c45a616b3b27b25965bc3e94f1
Quinton Lee <quivistis@gmail.com>
2020-09-13 02:06:38 -0400

    Pancham's Pairs: mark as non-trivial load

--- src/games/panchams-pairs.ts
+++ src/games/panchams-pairs.ts
@@ -314,6 +314,7 @@ export const game: IGameFile<PanchamPairs> = {
 		"generation, color, type, and ability.",
 	name: "Pancham's Pairs",
 	mascot: "Pancham",
+	nonTrivialLoadData: true,
 	variants: [
 		{
 			name: "Pancham's Move Pairs",
