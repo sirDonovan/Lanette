9a763f92e00ac2d8d9f4b99ed3b41387bec2edcf
Quinton Lee <quivistis@gmail.com>
2020-09-27 18:17:38 -0400

    Tapus' Terrains: add freejoin variant

--- src/games/tapus-terrains.ts
+++ src/games/tapus-terrains.ts
@@ -8,19 +8,19 @@ const terrains = {
 	'Basic': 'Normal',
 	'Cavernous': 'Dragon',
 	'Cloudy': 'Flying',
-	'Electric': 'Electric',
+	'Electrified': 'Electric',
 	'Grassy': 'Grass',
 	'Infested': 'Bug',
 	'Metallic': 'Steel',
 	'Misty': 'Fairy',
 	'Molten': 'Fire',
-	'Psychic': 'Psychic',
 	'Rocky': 'Rock',
 	'Sandy': 'Ground',
 	'Shady': 'Dark',
 	'Snowy': 'Ice',
 	'Spooky': 'Ghost',
 	'Swampy': 'Water',
+	'Twisted': 'Psychic',
 	'Venomous': 'Poison',
 	'Violent': 'Fighting',
 };
@@ -31,19 +31,19 @@ const data: {pokemon: KeyedDict<TerrainKey, string[]>} = {
 		'Basic': [],
 		'Cavernous': [],
 		'Cloudy': [],
-		'Electric': [],
+		'Electrified': [],
 		'Grassy': [],
 		'Infested': [],
 		'Metallic': [],
 		'Misty': [],
 		'Molten': [],
-		'Psychic': [],
 		'Rocky': [],
 		'Sandy': [],
 		'Shady': [],
 		'Snowy': [],
 		'Spooky': [],
 		'Swampy': [],
+		'Twisted': [],
 		'Venomous': [],
 		'Violent': [],
 	},
@@ -55,6 +55,7 @@ class TapusTerrains extends Game {
 	currentTerrain: TerrainKey | null = null;
 	firstJump: Player | null = null;
 	isElimination: boolean = false;
+	points = new Map<Player, number>();
 	queue: Player[] = [];
 	revealTime: number = 3.5 * 1000;
 	roundJumps = new Map<Player, boolean>();
@@ -83,6 +84,10 @@ class TapusTerrains extends Game {
 		return true;
 	}
 
+	onSignups(): void {
+		if (this.format.options.freejoin) this.timeout = setTimeout(() => this.nextRound(), 5000);
+	}
+
 	onStart(): void {
 		this.nextRound();
 	}
@@ -91,39 +96,48 @@ class TapusTerrains extends Game {
 		this.canJump = false;
 		if (this.round > 1 && this.targetPokemon && this.currentTerrain) {
 			if (!data.pokemon[this.currentTerrain].includes(this.targetPokemon)) {
-				for (const i in this.players) {
-					if (this.players[i].eliminated) continue;
-					if (this.queue.includes(this.players[i]) || this.roundJumps.has(this.players[i])) {
-						this.eliminatePlayer(this.players[i], "You jumped on a Pokemon of the wrong type!");
+				if (!this.format.options.freejoin) {
+					for (const i in this.players) {
+						if (this.players[i].eliminated) continue;
+						if (this.queue.includes(this.players[i]) || this.roundJumps.has(this.players[i])) {
+							this.eliminatePlayer(this.players[i], "You jumped on a Pokemon of the wrong type!");
+						}
 					}
 				}
 			} else {
 				this.currentTerrain = null;
-				const len = this.queue.length;
-				if (len > 1 && this.isElimination) {
-					this.eliminatePlayer(this.queue[len - 1], "You were the last player to jump on " + this.targetPokemon + "!");
-				}
-				for (const i in this.players) {
-					if (this.players[i].eliminated) continue;
-					if (!this.queue.includes(this.players[i])) {
-						this.eliminatePlayer(this.players[i], "You did not jump on " + this.targetPokemon + "!");
+				if (!this.format.options.freejoin) {
+					const len = this.queue.length;
+					if (len > 1 && this.isElimination) {
+						this.eliminatePlayer(this.queue[len - 1], "You were the last player to jump on " + this.targetPokemon + "!");
 					}
+					for (const i in this.players) {
+						if (this.players[i].eliminated) continue;
+						if (!this.queue.includes(this.players[i])) {
+							this.eliminatePlayer(this.players[i], "You did not jump on " + this.targetPokemon + "!");
+						}
+					}
+					// if (len) this.markFirstAction(this.queue[0], 'firstJump');
 				}
-				// if (len) this.markFirstAction(this.queue[0], 'firstJump');
 			}
-			if (this.getRemainingPlayerCount() < 2) return this.end();
+
+			if (!this.format.options.freejoin && this.getRemainingPlayerCount() < 2) return this.end();
 		}
+
 		let newTerrain = false;
 		if (!this.currentTerrain) {
 			this.currentTerrain = this.sampleOne(terrainKeys);
 			newTerrain = true;
 			this.terrainRound++;
-			if (this.revealTime > 2000) this.revealTime -= 500;
-			if (this.terrainRound === 20) {
-				this.end();
-				return;
+			if (!this.format.options.freejoin) {
+				if (this.revealTime > 2000) this.revealTime -= 500;
+				if (this.terrainRound === 20) {
+					this.end();
+					return;
+				}
 			}
 		}
+
 		let targetPokemon = '';
 		if (this.random(2)) {
 			targetPokemon = this.sampleOne(data.pokemon[this.currentTerrain]);
@@ -137,13 +151,15 @@ class TapusTerrains extends Game {
 			}
 		}
 		this.targetPokemon = targetPokemon;
-		this.roundJumps.clear();
 		this.queue = [];
+
+		if (!this.format.options.freejoin) this.roundJumps.clear();
 
 		const pokemonHtml = '<div class="infobox"><center>' + Dex.getPokemonGif(Dex.getExistingPokemon(this.targetPokemon)) +
 			'<br />A wild <b>' + this.targetPokemon + '</b> appeared!</center></div>';
 		if (newTerrain) {
-			const roundHtml = this.getRoundHtml(this.getPlayerNames, null, "Round " + this.terrainRound);
+			const roundHtml = this.getRoundHtml(this.format.options.freejoin ? this.getPlayerPoints : this.getPlayerNames, null,
+				"Round " + this.terrainRound);
 			const uhtmlName = this.uhtmlBaseName + '-round';
 			this.onUhtml(uhtmlName, roundHtml, () => {
 				const terrainHtml = '<div class="infobox"><center><br />The terrain is <b>' + this.currentTerrain + '</b> (jump on <b>' +
@@ -194,11 +210,32 @@ const commands: GameCommandDefinitions<TapusTerrains> = {
 	jump: {
 		// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 		command(target, room, user): GameCommandReturnType {
-			if (this.roundJumps.has(this.players[user.id])) return false;
-			const player = this.players[user.id];
+			const player = this.createPlayer(user) || this.players[user.id];
+			if (this.roundJumps.has(player)) return false;
 			this.roundJumps.set(player, true);
 			if (!this.canJump) return false;
-			this.queue.push(player);
+
+			if (this.format.options.freejoin) {
+				if (this.currentTerrain && this.targetPokemon && data.pokemon[this.currentTerrain].includes(this.targetPokemon)) {
+					if (this.timeout) clearTimeout(this.timeout);
+					this.currentTerrain = null;
+					const points = this.addPoints(player, 1);
+					if (points === this.format.options.points) {
+						for (const i in this.players) {
+							if (this.players[i] !== player) this.players[i].eliminated = true;
+						}
+						this.end();
+					} else {
+						this.say("**" + player.name + "** advances to **" + points + "** point" + (points > 1 ? "s" : "") + "!");
+						this.timeout = setTimeout(() => {
+							this.roundJumps.clear();
+							this.nextRound();
+						}, 5000);
+					}
+				}
+			} else {
+				this.queue.push(player);
+			}
 			return true;
 		},
 	},
@@ -210,6 +247,7 @@ export const game: IGameFile<TapusTerrains> = {
 	class: TapusTerrains,
 	commandDescriptions: [Config.commandCharacter + 'jump'],
 	commands,
+	defaultOptions: ['freejoin', 'points'],
 	description: "Players race through various terrains on Pokemon! Only jump on Pokemon of the appropriate type in each terrain.",
 	formerNames: ["Terrain Race"],
 	name: "Tapus' Terrains",
