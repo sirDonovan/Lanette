32b6c6b317662f8a108ff10fd66499179514f745
ZardMX <58093734+ZardMX@users.noreply.github.com>
2020-10-11 22:00:53 +0530

    Unown's Pokemon Chain: fix description typo (#16)

--- src/games/unowns-pokemon-chain.ts
+++ src/games/unowns-pokemon-chain.ts
@@ -15,8 +15,8 @@ export const game: IGameFile<UnownsPokemonChain> = Games.copyTemplateProperties(
 	variants: [
 		{
 			name: "Unown's Ability Chain",
-			description: "Players answer each round with a move that starts with the last letter of the previous ability (no repeats in " +
-				"a round)!",
+			description: "Players answer each round with an ability that starts with the last letter of the previous ability (no repeats " +
+				"in a round)!",
 			commandDescriptions: [Config.commandCharacter + "g [ability]"],
 			variant: "abilities",
 			variantAliases: ['ability'],
