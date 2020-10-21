3cd64c4a44d612d9a2c509991c64867f7c91b70e
Quinton Lee <quivistis@gmail.com>
2020-10-21 12:41:20 -0500

    Fix spacing and reword

--- src/commands.ts
+++ src/commands.ts
@@ -3684,8 +3684,9 @@ const commands: CommandDefinitions<CommandContext> = {
 			}
 
 			users = users.filter(x => pointsCache[x] !== 0).sort((a, b) => pointsCache[b] - pointsCache[a]);
-			if (!users.length) return this.say("The " + (source ? source.name : "") + " leaderboard for the " +
-				leaderboardRoom.title + " room is empty.");
+			if (!users.length) {
+				return this.say("The " + leaderboardRoom.title + (source ? " " + source.name : "") + " leaderboard is empty.");
+			}
 
 			const output: string[] = [];
 			const positions = 10;
