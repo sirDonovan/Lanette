9f3619a3cd2eb0b55ed7c02c764471c9db54dc60
Quinton Lee <quivistis@gmail.com>
2020-11-21 18:44:01 -0600

    Serperiors Lengthy Chains: add moves variant

--- src/games/serperiors-lengthy-chains.ts
+++ src/games/serperiors-lengthy-chains.ts
@@ -2,20 +2,43 @@ import type { Player } from "../room-activity";
 import { ScriptedGame } from "../room-game-scripted";
 import type { GameCommandDefinitions, IGameFile } from "../types/games";
 
-const minimumParameterPokemon = 16;
+const minimumParameterResults = 15;
 
-const data: {parameters: Dict<string[]>; parameterKeys: string[]} = {
-	parameters: {},
-	parameterKeys: [],
+type DataTypes = 'pokemon' | 'moves';
+const data: {parameters: KeyedDict<DataTypes, Dict<string[]>>; parameterKeys: KeyedDict<DataTypes, string[]>} = {
+	parameters: {
+		pokemon: {},
+		moves: {},
+	},
+	parameterKeys: {
+		pokemon: [],
+		moves: [],
+	},
 };
 
 class SerperiorLengthyChains extends ScriptedGame {
 	bestChain: string[] = [];
 	bestPlayer: Player | null = null;
 	category: string = '';
+	dataType: DataTypes = 'pokemon';
 	points = new Map<Player, number>();
 
 	static loadData(): void {
+		for (const move of Games.getMovesList()) {
+			const moveParameters: string[] = [move.category, "Generation " + move.gen, move.pp + " Power Points", move.type + " Type"];
+			if (move.accuracy === true) {
+				moveParameters.push("Does not check accuracy");
+			} else {
+				moveParameters.push(move.accuracy + "% Accuracy");
+			}
+
+			if (!move.basePowerCallback) moveParameters.push(move.basePower + " Base Power");
+			for (const param of moveParameters) {
+				if (!(param in data.parameters.moves)) data.parameters.moves[param] = [];
+				data.parameters.moves[param].push(move.id);
+			}
+		}
+
 		for (const pokemon of Games.getPokemonList()) {
 			const pokemonParameters: string[] = ["Generation " + pokemon.gen, pokemon.color];
 			if (Games.isIncludedPokemonTier(pokemon.tier)) pokemonParameters.push(pokemon.tier);
@@ -26,35 +49,37 @@ class SerperiorLengthyChains extends ScriptedGame {
 				pokemonParameters.push(type + " Type");
 			}
 			for (const param of pokemonParameters) {
-				if (!(param in data.parameters)) data.parameters[param] = [];
-				data.parameters[param].push(pokemon.id);
+				if (!(param in data.parameters.pokemon)) data.parameters.pokemon[param] = [];
+				data.parameters.pokemon[param].push(pokemon.id);
 			}
 		}
 
-		const parametersKeys = Object.keys(data.parameters);
-		const len = parametersKeys.length;
-		for (let i = 0; i < len; i++) {
-			for (let j = 0; j < len; j++) {
-				if (i === j || data.parameters[parametersKeys[i]].length < minimumParameterPokemon ||
-					data.parameters[parametersKeys[j]].length < minimumParameterPokemon) continue;
+		for (const dataType of ['pokemon', 'moves'] as DataTypes[]) {
+			const parametersKeys = Object.keys(data.parameters[dataType]);
+			const len = parametersKeys.length;
+			for (let i = 0; i < len; i++) {
+				for (let j = 0; j < len; j++) {
+					if (i === j || data.parameters[dataType][parametersKeys[i]].length < minimumParameterResults ||
+						data.parameters[dataType][parametersKeys[j]].length < minimumParameterResults) continue;
 
-				const paramA = parametersKeys[i];
-				const paramB = parametersKeys[j];
-				const combined = paramA + ", " + paramB;
-				data.parameters[combined] = [];
-				for (const pokemon of data.parameters[paramA]) {
-					if (data.parameters[paramB].includes(pokemon)) {
-						data.parameters[combined].push(pokemon);
+					const paramA = parametersKeys[i];
+					const paramB = parametersKeys[j];
+					const combined = paramA + ", " + paramB;
+					data.parameters[dataType][combined] = [];
+					for (const pokemon of data.parameters[dataType][paramA]) {
+						if (data.parameters[dataType][paramB].includes(pokemon)) {
+							data.parameters[dataType][combined].push(pokemon);
+						}
 					}
 				}
 			}
-		}
 
-		for (const param in data.parameters) {
-			if (data.parameters[param].length < minimumParameterPokemon) {
-				delete data.parameters[param];
-			} else {
-				data.parameterKeys.push(param);
+			for (const param in data.parameters[dataType]) {
+				if (data.parameters[dataType][param].length < minimumParameterResults) {
+					delete data.parameters[dataType][param];
+				} else {
+					data.parameterKeys[dataType].push(param);
+				}
 			}
 		}
 	}
@@ -66,8 +91,8 @@ class SerperiorLengthyChains extends ScriptedGame {
 	onNextRound(): void {
 		this.bestChain = [];
 		this.bestPlayer = null;
-		this.category = this.sampleOne(data.parameterKeys);
-		this.say("Make a chain of **" + this.category + "** Pokemon!");
+		this.category = this.sampleOne(data.parameterKeys[this.dataType]);
+		this.say("Make a chain of **" + this.category + "** " + (this.dataType === 'moves' ? "moves" : "Pokemon") + "!");
 		this.timeout = setTimeout(() => this.checkBestChain(), 15 * 1000);
 	}
 
@@ -96,15 +121,25 @@ class SerperiorLengthyChains extends ScriptedGame {
 	getChain(guess: string, chainSoFar: string[]): string[] {
 		if (!guess || guess.length === 1) return chainSoFar;
 		let chain = [];
+		const moves = this.dataType === 'moves';
 		for (let i = guess.length - 1; i > 0; i--) {
 			const substr = guess.substr(0, i + 1);
-			if (data.parameters[this.category].includes(substr)) {
-				const pokemon = Dex.getExistingPokemon(substr);
-				if (chainSoFar.includes(pokemon.name)) {
-					return chainSoFar;
-				}
+			if (data.parameters[this.dataType][this.category].includes(substr)) {
 				const curChain = chainSoFar.slice();
-				curChain.push(pokemon.name);
+				if (moves) {
+					const move = Dex.getExistingMove(substr);
+					if (chainSoFar.includes(move.name)) {
+						return chainSoFar;
+					}
+					curChain.push(move.name);
+				} else {
+					const pokemon = Dex.getExistingPokemon(substr);
+					if (chainSoFar.includes(pokemon.name)) {
+						return chainSoFar;
+					}
+					curChain.push(pokemon.name);
+				}
+
 				chain = this.getChain(guess.substr(i), curChain);
 				if (chain.length) return chain;
 			}
@@ -134,14 +169,23 @@ const commands: GameCommandDefinitions<SerperiorLengthyChains> = {
 export const game: IGameFile<SerperiorLengthyChains> = {
 	aliases: ["serperiors", "slc"],
 	category: 'puzzle',
-	commandDescriptions: [Config.commandCharacter + "g [Pokemon chain]"],
+	commandDescriptions: [Config.commandCharacter + "g [chain]"],
 	commands,
 	class: SerperiorLengthyChains,
 	defaultOptions: ['points'],
-	description: "Player's form chains of Pokemon that follow the given parameter! A chain is a sequence of pokemon that share 1 letter " +
+	description: "Player's form chains of Pokemon that follow the given parameters! A chain is a sequence of Pokemon that share 1 letter " +
 		"(such as PikachUxie)!",
 	freejoin: true,
 	name: "Serperior's Lengthy Chains",
 	mascot: "Serperior",
 	nonTrivialLoadData: true,
+	variants: [
+		{
+			name: "Serperior's Lengthy Move Chains",
+			description: "Player's form chains of moves that follow the given parameters! A chain is a sequence of moves that share 1 " +
+				"letter (such as AirSlasHealBlock)!",
+			variantAliases: ['move', 'moves'],
+			dataType: 'moves',
+		},
+	],
 };
