c52fd126b7e8a08eceb8f09a42e5a0b813f930fd
Parth Kudal <76895798+Frozoid@users.noreply.github.com>
2021-02-14 23:42:09 +0530

    locations

--- src/games/ambipoms-tossups.ts
+++ src/games/ambipoms-tossups.ts
@@ -2,11 +2,13 @@ import type { Player } from "../room-activity";
 import type { IGameFile } from "../types/games";
 import { game as questionAndAnswerGame, QuestionAndAnswer } from './templates/question-and-answer';
 
-const data: {'Pokemon': string[]; 'Pokemon Abilities': string[]; 'Pokemon Items': string[]; 'Pokemon Moves': string[]} = {
+// eslint-disable-next-line max-len
+const data: {'Pokemon': string[]; 'Pokemon Abilities': string[]; 'Pokemon Items': string[]; 'Pokemon Moves': string[];'Pokemon Locations': string[]} = {
 	"Pokemon": [],
 	"Pokemon Abilities": [],
 	"Pokemon Items": [],
 	"Pokemon Moves": [],
+	"Pokemon Locations": [],
 };
 type DataKey = keyof typeof data;
 const categories = Object.keys(data) as DataKey[];
@@ -27,6 +29,8 @@ class AmbipomsTossups extends QuestionAndAnswer {
 	updateHintTime: number = 5 * 1000;
 
 	static loadData(): void {
+		// eslint-disable-next-line max-len
+		data["Pokemon Locations"] = Dex.getLocations().filter(x => x.length > 3);data["Pokemon Locations"] = Dex.getLocations().filter(x => x.length > 3);
 		data["Pokemon"] = Games.getPokemonList().filter(x => x.name.length < 18).map(x => x.name);
 		data["Pokemon Abilities"] = Games.getAbilitiesList().filter(x => x.name.length < 18).map(x => x.name);
 		data["Pokemon Items"] = Games.getItemsList().filter(x => x.name.length < 18).map(x => x.name);
@@ -139,6 +143,11 @@ export const game: IGameFile<AmbipomsTossups> = Games.copyTemplateProperties(que
 			name: "Ambipom's Move Tossups",
 			roundCategory: "Pokemon Moves",
 			variantAliases: ['move', 'moves'],
+		},
+		{
+			name: "Ambipom's Location tossups",
+			roundCategory: "Pokemon Locations",
+			variantAliases: ['location', 'locations'],
 		},
 	],
 });
