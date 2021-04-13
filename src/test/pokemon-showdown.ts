9957bb502f884b63ee2c676470e96d5dbb4c396f
Quinton Lee <quivistis@gmail.com>
2021-04-12 21:26:46 -0400

    Test: check data keys in pokemon-showdown

--- src/test/pokemon-showdown.ts
+++ src/test/pokemon-showdown.ts
@@ -5,7 +5,16 @@ const formatEffectTypes: string[] = ['Format', 'Rule', 'ValidatorRule'];
 /* eslint-env mocha */
 describe("pokemon-showdown", () => {
 	it("should properly interface with Lanette", () => {
-		for (const i of Dex.getData().abilityKeys) {
+		/*
+			Abilities
+		*/
+
+		const abilityKeys = Dex.getData().abilityKeys;
+		assert(abilityKeys.length);
+		assert(abilityKeys.includes('stench'));
+		assert(abilityKeys.includes('mountaineer'));
+
+		for (const i of abilityKeys) {
 			const ability = Dex.getAbility(i);
 			assert(ability, i);
 			assertStrictEqual(ability.id, i);
@@ -16,132 +25,6 @@ describe("pokemon-showdown", () => {
 			if (ability.desc) assertStrictEqual(typeof ability.desc, 'string');
 			if (ability.shortDesc) assertStrictEqual(typeof ability.shortDesc, 'string');
 		}
-
-		for (const i of Dex.getData().formatKeys) {
-			const format = Dex.getFormat(i);
-			assert(format, i);
-			assertStrictEqual(format.id, i);
-			assert(formatEffectTypes.includes(format.effectType));
-			assertStrictEqual(typeof format.name, 'string');
-			assert(Array.isArray(format.banlist));
-			if (format.banlist.length) assertStrictEqual(typeof format.banlist[0], 'string');
-
-			assert(Array.isArray(format.unbanlist));
-			if (format.unbanlist.length) assertStrictEqual(typeof format.unbanlist[0], 'string');
-
-			assert(Array.isArray(format.restricted));
-			if (format.restricted.length) assertStrictEqual(typeof format.restricted[0], 'string');
-
-			assert(Array.isArray(format.ruleset));
-			if (format.ruleset.length) assertStrictEqual(typeof format.ruleset[0], 'string');
-		}
-
-		for (const i of Dex.getData().itemKeys) {
-			const item = Dex.getItem(i);
-			assert(item, i);
-			assertStrictEqual(item.id, i);
-			assertStrictEqual(item.effectType, "Item");
-			assertStrictEqual(typeof item.name, 'string');
-			assertStrictEqual(typeof item.gen, 'number');
-			if (item.desc) assertStrictEqual(typeof item.desc, 'string');
-			if (item.shortDesc) assertStrictEqual(typeof item.shortDesc, 'string');
-			if (item.isNonstandard) assertStrictEqual(typeof item.isNonstandard, 'string');
-		}
-
-		for (const i of Dex.getData().learnsetDataKeys) {
-			assert(Dex.getLearnsetData(i), i);
-		}
-
-		for (const i of Dex.getData().moveKeys) {
-			const move = Dex.getMove(i);
-			assert(move, i);
-			if (move.realMove) {
-				assertStrictEqual(i, Tools.toId(move.name));
-				assertStrictEqual(move.id, Tools.toId(move.realMove));
-			} else {
-				assertStrictEqual(move.id, i);
-			}
-			assertStrictEqual(move.effectType, "Move");
-			assertStrictEqual(typeof move.name, 'string');
-			assertStrictEqual(typeof move.gen, 'number');
-			assertStrictEqual(typeof move.type, 'string');
-			if (move.desc) assertStrictEqual(typeof move.desc, 'string');
-			if (move.shortDesc) assertStrictEqual(typeof move.shortDesc, 'string');
-			if (move.isNonstandard) assertStrictEqual(typeof move.isNonstandard, 'string');
-		}
-
-		for (const i of Dex.getData().natureKeys) {
-			const nature = Dex.getNature(i);
-			assert(nature, i);
-			assertStrictEqual(nature.id, i);
-			assertStrictEqual(nature.effectType, "Nature");
-			assertStrictEqual(typeof nature.name, 'string');
-			assertStrictEqual(typeof nature.gen, 'number');
-			if (nature.plus) assertStrictEqual(typeof nature.plus, 'string');
-			if (nature.minus) assertStrictEqual(typeof nature.minus, 'string');
-		}
-
-		for (const i of Dex.getData().pokemonKeys) {
-			const pokemon = Dex.getPokemon(i);
-			assert(pokemon, i);
-			assertStrictEqual(pokemon.id, i);
-			assertStrictEqual(pokemon.effectType, "Pokemon");
-			assertStrictEqual(typeof pokemon.name, 'string');
-			assertStrictEqual(typeof pokemon.baseSpecies, 'string');
-			assertStrictEqual(typeof pokemon.gen, 'number');
-			assertStrictEqual(typeof pokemon.num, 'number');
-
-			assert(Array.isArray(pokemon.types));
-			assertStrictEqual(typeof pokemon.types[0], 'string');
-
-			assert(Array.isArray(pokemon.evos));
-			if (pokemon.evos.length) assertStrictEqual(typeof pokemon.evos[0], 'string');
-
-			assert(Array.isArray(pokemon.eggGroups));
-			if (pokemon.eggGroups.length) assertStrictEqual(typeof pokemon.eggGroups[0], 'string');
-
-			assertStrictEqual(typeof pokemon.spriteid, 'string');
-			assertStrictEqual(typeof pokemon.abilities['0'], 'string');
-			assertStrictEqual(typeof pokemon.baseStats['hp'], 'number');
-			assertStrictEqual(typeof pokemon.baseStats['atk'], 'number');
-			assertStrictEqual(typeof pokemon.baseStats['def'], 'number');
-			assertStrictEqual(typeof pokemon.baseStats['spa'], 'number');
-			assertStrictEqual(typeof pokemon.baseStats['spd'], 'number');
-			assertStrictEqual(typeof pokemon.baseStats['spe'], 'number');
-			assertStrictEqual(typeof pokemon.tier, 'string');
-			assertStrictEqual(typeof pokemon.prevo, 'string');
-			// @ts-expect-error
-			if (pokemon.nfe !== 0) {
-				assertStrictEqual(typeof pokemon.nfe, 'boolean');
-			}
-			if (pokemon.isNonstandard) assertStrictEqual(typeof pokemon.isNonstandard, 'string');
-			if (pokemon.forme) assertStrictEqual(typeof pokemon.forme, 'string');
-			if (pokemon.evoLevel) assertStrictEqual(typeof pokemon.evoLevel, 'number');
-			if (pokemon.evoMove) assertStrictEqual(typeof pokemon.evoMove, 'string');
-			if (pokemon.evoCondition) assertStrictEqual(typeof pokemon.evoCondition, 'string');
-			if (pokemon.evoItem) assertStrictEqual(typeof pokemon.evoItem, 'string');
-			if (pokemon.evoType) assertStrictEqual(typeof pokemon.evoType, 'string');
-			if (pokemon.cosmeticFormes) {
-				assert(Array.isArray(pokemon.cosmeticFormes));
-				assertStrictEqual(typeof pokemon.cosmeticFormes[0], 'string');
-			}
-			if (pokemon.otherFormes) {
-				assert(Array.isArray(pokemon.otherFormes));
-				assertStrictEqual(typeof pokemon.otherFormes[0], 'string');
-			}
-		}
-
-		for (const i of Dex.getData().typeKeys) {
-			const type = Dex.getType(i);
-			assert(type, i);
-			assertStrictEqual(type.id, i);
-			assertStrictEqual(typeof type.name, 'string');
-			assertStrictEqual(typeof type.damageTaken, 'object');
-		}
-
-		/*
-			Abilities
-		*/
 
 		assertStrictEqual(Dex.getExistingAbility("Stench").gen, 3);
 		assertStrictEqual(Dex.getExistingAbility("Tangled Feet").gen, 4);
@@ -163,8 +46,63 @@ describe("pokemon-showdown", () => {
 		assertStrictEqual(capAbility.isNonstandard, "CAP");
 
 		/*
+			Formats
+		*/
+
+		const formatKeys = Dex.getData().formatKeys;
+		assert(formatKeys.length);
+		assert(formatKeys.includes('gen8ou'));
+		assert(formatKeys.includes('gen1ou'));
+
+		for (const i of formatKeys) {
+			const format = Dex.getFormat(i);
+			assert(format, i);
+			assertStrictEqual(format.id, i);
+			assert(formatEffectTypes.includes(format.effectType));
+			assertStrictEqual(typeof format.name, 'string');
+			assert(Array.isArray(format.banlist));
+			if (format.banlist.length) assertStrictEqual(typeof format.banlist[0], 'string');
+
+			assert(Array.isArray(format.unbanlist));
+			if (format.unbanlist.length) assertStrictEqual(typeof format.unbanlist[0], 'string');
+
+			assert(Array.isArray(format.restricted));
+			if (format.restricted.length) assertStrictEqual(typeof format.restricted[0], 'string');
+
+			assert(Array.isArray(format.ruleset));
+			if (format.ruleset.length) assertStrictEqual(typeof format.ruleset[0], 'string');
+		}
+
+		const currentGenFormat = Dex.getFormat("gen8ou");
+		assert(currentGenFormat);
+		assertStrictEqual(currentGenFormat.id, 'gen8ou');
+		assertStrictEqual(currentGenFormat.name, "[Gen 8] OU");
+
+		const pastGenFormat = Dex.getFormat("gen1ou");
+		assert(pastGenFormat);
+		assertStrictEqual(pastGenFormat.id, 'gen1ou');
+		assertStrictEqual(pastGenFormat.name, "[Gen 1] OU");
+
+		/*
 			Items
 		*/
+
+		const itemKeys = Dex.getData().itemKeys;
+		assert(itemKeys.length);
+		assert(itemKeys.includes('berryjuice'));
+		assert(itemKeys.includes('crucibellite'));
+
+		for (const i of itemKeys) {
+			const item = Dex.getItem(i);
+			assert(item, i);
+			assertStrictEqual(item.id, i);
+			assertStrictEqual(item.effectType, "Item");
+			assertStrictEqual(typeof item.name, 'string');
+			assertStrictEqual(typeof item.gen, 'number');
+			if (item.desc) assertStrictEqual(typeof item.desc, 'string');
+			if (item.shortDesc) assertStrictEqual(typeof item.shortDesc, 'string');
+			if (item.isNonstandard) assertStrictEqual(typeof item.isNonstandard, 'string');
+		}
 
 		assertStrictEqual(Dex.getExistingItem("Berry Juice").gen, 2);
 		assertStrictEqual(Dex.getExistingItem("Aguav Berry").gen, 3);
@@ -233,8 +171,45 @@ describe("pokemon-showdown", () => {
 		assertStrictEqual(zCrystal.zMoveType, "Bug");
 
 		/*
+			Learnset data
+		*/
+
+		const learnsetDataKeys = Dex.getData().learnsetDataKeys;
+		assert(learnsetDataKeys.length);
+		assert(learnsetDataKeys.includes('pikachu'));
+		assert(learnsetDataKeys.includes('pokestarsmeargle'));
+		assert(learnsetDataKeys.includes('arghonaut'));
+
+		for (const i of learnsetDataKeys) {
+			assert(Dex.getLearnsetData(i), i);
+		}
+
+		/*
 			Moves
 		*/
+
+		const moveKeys = Dex.getData().moveKeys;
+		assert(moveKeys.length);
+		assert(moveKeys.includes('pound'));
+		assert(moveKeys.includes('paleowave'));
+
+		for (const i of moveKeys) {
+			const move = Dex.getMove(i);
+			assert(move, i);
+			if (move.realMove) {
+				assertStrictEqual(i, Tools.toId(move.name));
+				assertStrictEqual(move.id, Tools.toId(move.realMove));
+			} else {
+				assertStrictEqual(move.id, i);
+			}
+			assertStrictEqual(move.effectType, "Move");
+			assertStrictEqual(typeof move.name, 'string');
+			assertStrictEqual(typeof move.gen, 'number');
+			assertStrictEqual(typeof move.type, 'string');
+			if (move.desc) assertStrictEqual(typeof move.desc, 'string');
+			if (move.shortDesc) assertStrictEqual(typeof move.shortDesc, 'string');
+			if (move.isNonstandard) assertStrictEqual(typeof move.isNonstandard, 'string');
+		}
 
 		assertStrictEqual(Dex.getExistingMove("Pound").gen, 1);
 		assertStrictEqual(Dex.getExistingMove("Sketch").gen, 2);
@@ -245,9 +220,9 @@ describe("pokemon-showdown", () => {
 		assertStrictEqual(Dex.getExistingMove("Breakneck Blitz").gen, 7);
 		assertStrictEqual(Dex.getExistingMove("Max Guard").gen, 8);
 
-		const type = Dex.getMove("Tackle");
-		assert(type);
-		assertStrictEqual(type.type, "Normal");
+		const moveType = Dex.getMove("Tackle");
+		assert(moveType);
+		assertStrictEqual(moveType.type, "Normal");
 
 		const basePowerCallback = Dex.getMove("Acrobatics");
 		assert(basePowerCallback);
@@ -283,9 +258,79 @@ describe("pokemon-showdown", () => {
 		assert(oneHundredPercentAccuracy);
 		assertStrictEqual(oneHundredPercentAccuracy.accuracy, 100);
 
+		const natureKeys = Dex.getData().natureKeys;
+		assert(natureKeys.length);
+
+		for (const i of natureKeys) {
+			const nature = Dex.getNature(i);
+			assert(nature, i);
+			assertStrictEqual(nature.id, i);
+			assertStrictEqual(nature.effectType, "Nature");
+			assertStrictEqual(typeof nature.name, 'string');
+			assertStrictEqual(typeof nature.gen, 'number');
+			if (nature.plus) assertStrictEqual(typeof nature.plus, 'string');
+			if (nature.minus) assertStrictEqual(typeof nature.minus, 'string');
+		}
+
 		/*
 			Pokemon
 		*/
+
+		const pokemonKeys = Dex.getData().pokemonKeys;
+		assert(pokemonKeys.length);
+		assert(pokemonKeys.includes('pikachu'));
+		assert(pokemonKeys.includes('pokestarsmeargle'));
+		assert(pokemonKeys.includes('arghonaut'));
+
+		for (const i of pokemonKeys) {
+			const pokemon = Dex.getPokemon(i);
+			assert(pokemon, i);
+			assertStrictEqual(pokemon.id, i);
+			assertStrictEqual(pokemon.effectType, "Pokemon");
+			assertStrictEqual(typeof pokemon.name, 'string');
+			assertStrictEqual(typeof pokemon.baseSpecies, 'string');
+			assertStrictEqual(typeof pokemon.gen, 'number');
+			assertStrictEqual(typeof pokemon.num, 'number');
+
+			assert(Array.isArray(pokemon.types));
+			assertStrictEqual(typeof pokemon.types[0], 'string');
+
+			assert(Array.isArray(pokemon.evos));
+			if (pokemon.evos.length) assertStrictEqual(typeof pokemon.evos[0], 'string');
+
+			assert(Array.isArray(pokemon.eggGroups));
+			if (pokemon.eggGroups.length) assertStrictEqual(typeof pokemon.eggGroups[0], 'string');
+
+			assertStrictEqual(typeof pokemon.spriteid, 'string');
+			assertStrictEqual(typeof pokemon.abilities['0'], 'string');
+			assertStrictEqual(typeof pokemon.baseStats['hp'], 'number');
+			assertStrictEqual(typeof pokemon.baseStats['atk'], 'number');
+			assertStrictEqual(typeof pokemon.baseStats['def'], 'number');
+			assertStrictEqual(typeof pokemon.baseStats['spa'], 'number');
+			assertStrictEqual(typeof pokemon.baseStats['spd'], 'number');
+			assertStrictEqual(typeof pokemon.baseStats['spe'], 'number');
+			assertStrictEqual(typeof pokemon.tier, 'string');
+			assertStrictEqual(typeof pokemon.prevo, 'string');
+			// @ts-expect-error
+			if (pokemon.nfe !== 0) {
+				assertStrictEqual(typeof pokemon.nfe, 'boolean');
+			}
+			if (pokemon.isNonstandard) assertStrictEqual(typeof pokemon.isNonstandard, 'string');
+			if (pokemon.forme) assertStrictEqual(typeof pokemon.forme, 'string');
+			if (pokemon.evoLevel) assertStrictEqual(typeof pokemon.evoLevel, 'number');
+			if (pokemon.evoMove) assertStrictEqual(typeof pokemon.evoMove, 'string');
+			if (pokemon.evoCondition) assertStrictEqual(typeof pokemon.evoCondition, 'string');
+			if (pokemon.evoItem) assertStrictEqual(typeof pokemon.evoItem, 'string');
+			if (pokemon.evoType) assertStrictEqual(typeof pokemon.evoType, 'string');
+			if (pokemon.cosmeticFormes) {
+				assert(Array.isArray(pokemon.cosmeticFormes));
+				assertStrictEqual(typeof pokemon.cosmeticFormes[0], 'string');
+			}
+			if (pokemon.otherFormes) {
+				assert(Array.isArray(pokemon.otherFormes));
+				assertStrictEqual(typeof pokemon.otherFormes[0], 'string');
+			}
+		}
 
 		assertStrictEqual(Dex.getExistingPokemon("Bulbasaur").gen, 1);
 		assertStrictEqual(Dex.getExistingPokemon("Chikorita").gen, 2);
@@ -383,6 +428,40 @@ describe("pokemon-showdown", () => {
 		assert(hyphenName);
 		assertStrictEqual(hyphenName.id, "kommoo");
 		assertStrictEqual(hyphenName.name, "Kommo-o");
+
+		const pokestar = Dex.getPokemon("Pokestar Smeargle");
+		assert(pokestar);
+		assertStrictEqual(pokestar.id, "pokestarsmeargle");
+		assertStrictEqual(pokestar.name, "Pokestar Smeargle");
+
+		const cap = Dex.getPokemon("Arghonaut");
+		assert(cap);
+		assertStrictEqual(cap.id, "arghonaut");
+		assertStrictEqual(cap.name, "Arghonaut");
+
+		/*
+			Types
+		*/
+
+		const typeKeys = Dex.getData().typeKeys;
+		assert(typeKeys.length);
+		assert(typeKeys.includes('normal'));
+
+		for (const i of typeKeys) {
+			const type = Dex.getType(i);
+			assert(type, i);
+			assertStrictEqual(type.id, i);
+			assertStrictEqual(typeof type.name, 'string');
+			assertStrictEqual(typeof type.damageTaken, 'object');
+		}
+
+		const normalType = Dex.getType("Normal");
+		assert(normalType);
+		assertStrictEqual(normalType.id, 'normal');
+		assertStrictEqual(normalType.name, "Normal");
+		assertStrictEqual(normalType.damageTaken['Normal'], 0);
+		assertStrictEqual(normalType.damageTaken['Fighting'], 1);
+		assertStrictEqual(normalType.damageTaken['Ghost'], 3);
 
 		const allPossibleMoves = Dex.getAllPossibleMoves(Dex.getExistingPokemon("Charizard"));
 		assert(allPossibleMoves);
