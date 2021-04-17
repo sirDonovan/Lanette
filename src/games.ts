ebcabb749ec0325ac660cfe3c26dd87a192736c8
Quinton Lee <quivistis@gmail.com>
2021-04-16 20:33:10 -0400

    Games: disallow modes in minigames

--- src/games.ts
+++ src/games.ts
@@ -564,6 +564,7 @@ export class Games {
 					const format = global.Games.getFormat(formatName + (target ? "," + target : ""), true);
 					if (Array.isArray(format)) return this.sayError(format);
 					if (global.Games.isReloadInProgress()) return this.sayError(['reloadInProgress']);
+					if (format.mode) return this.say("Minigames cannot be played in modes.");
 					delete format.inputOptions.points;
 					const game = global.Games.createGame(room, format, pmRoom, true);
 					game.signups();
