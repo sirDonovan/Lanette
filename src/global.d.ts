9be9a2ac5fd2cb6696c7fe4d44a3d0a53aaca3be
Quinton Lee <quivistis@gmail.com>
2021-12-14 21:38:21 -0500

    Update dependencies

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,31 +1,17 @@
-import type { Client as clientType } from './client';
-import type { CommandParser as commandParserType } from './command-parser';
-import type * as config from './config-example';
-import type { Dex as dexType } from './dex';
-import type { Games as gamesType } from './games';
-import type { Rooms as roomsType } from './rooms';
-import type { Storage as storageType } from './storage';
-import type { Tools as toolsType } from './tools';
-import type { Tournaments as tournamentsType } from './tournaments';
-import type { BaseLoadedCommands } from "./types/command-parser";
-import type { Users as usersType } from './users';
-
-/* eslint-disable @typescript-eslint/naming-convention */
-declare global {
-	const BaseCommands: BaseLoadedCommands;
-	const Client: clientType;
-	const CommandParser: commandParserType;
-	const Commands: BaseLoadedCommands;
-	const Config: Partial<typeof config>;
-	const Dex: dexType;
-	const Games: gamesType;
-	const __reloadInProgress: boolean;
-	const __reloadModules: (username: string, modules: string[]) => Promise<void>;
-	const Rooms: roomsType;
-	const Storage: storageType;
-	const tempConfig: boolean;
-	const Tools: toolsType;
-	const Tournaments: tournamentsType;
-	const Users: usersType;
-}
+/* eslint-disable no-var, @typescript-eslint/naming-convention */
+declare var BaseCommands: import('./types/command-parser').BaseLoadedCommands;
+declare var Client: import('./client').Client;
+declare var CommandParser: import('./command-parser').CommandParser;
+declare var Commands: import('./types/command-parser').BaseLoadedCommands;
+declare var Config: Partial<typeof import('./config-example')>;
+declare var Dex: import('./dex').Dex;
+declare var Games: import('./games').Games;
+declare var __reloadInProgress: boolean;
+declare var __reloadModules: (username: string, modules: string[]) => Promise<void>;
+declare var Rooms: import('./rooms').Rooms;
+declare var Storage: import('./storage').Storage;
+declare var tempConfig: boolean;
+declare var Tools: import('./tools').Tools;
+declare var Tournaments: import('./tournaments').Tournaments;
+declare var Users: import('./users').Users;
 /* eslint-enable */
