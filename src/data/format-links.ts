35c10555848929714679867d50648aadd07b5d48
Frixel <101074724+Frixel@users.noreply.github.com>
2022-05-17 01:52:38 +0100

    Data: add Pokebilities AAA teams (#52)

--- src/data/format-links.ts
+++ src/data/format-links.ts
@@ -123,6 +123,9 @@ export const formatLinks: Dict<IFormatDataLinks> = {
 	'gen8crossevolution': {
 		teams: 'https://www.smogon.com/forums/threads/3657562/#post-8308365',
 	},
+	'gen8pokebilitiesaaa': {
+		teams: 'https://www.smogon.com/forums/threads/3657159/page-22#post-8821047',
+	},
 
 	// Gen 7
 	'gen7letsgoou': {
