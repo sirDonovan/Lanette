8c2d5635156029f871c9d080b9bb1ce089a165e7
Quinton Lee <quivistis@gmail.com>
2022-06-14 20:50:54 -0500

    Games: prevent joining challenges and minigames

--- src/games/internal/egg-toss.ts
+++ src/games/internal/egg-toss.ts
@@ -14,6 +14,7 @@ class EggToss extends ScriptedGame {
 	currentHolder: Player | null = null;
 	explodeTimeout: NodeJS.Timer | null = null;
 	internalGame: boolean = true;
+	managedPlayers = true;
 	lastHolder: Player | null = null;
 
 	// hack for selectUser()
