7aa6adc4fb62cfe9015e15b4467c675cdf3e10ff
Quinton Lee <quivistis@gmail.com>
2022-09-18 22:38:04 -0400

    Add Doubles Release & D/Evolve

---
+++ src/games/doubles-release-and-devolve.ts
@@ -0,0 +1,56 @@
+import type { IGameFile } from '../types/games';
+import type { GameType, IPokemon } from '../types/pokemon-showdown';
+import { game as eliminationTournamentGame } from './templates/battle-elimination';
+import { BattleEliminationTournament } from './templates/battle-elimination-tournament';
+
+const name = "Doubles Release and De-volve";
+const description = "Every player is given a randomly generated team to start out. Each battle that you win, you " +
+	"must 'release' 2 of your Pokemon (remove them from your team) and then de-volve 2 Pokemon on your team.";
+
+class DoublesReleaseAndDevolve extends BattleEliminationTournament {
+	canChangeFormat = true;
+	firstRoundExtraTime = 5 * 60 * 1000;
+	activityWarnTimeout: number = 5 * 60 * 1000;
+	autoDqMinutes: number = 5;
+	dropsPerRound = 2;
+	evolutionsPerRound = -2;
+	startingTeamsLength = 6;
+	maxPlayers = 64;
+	minTeamSize = 2;
+	battleFormatId = 'doublesou';
+	battleFormatType: GameType = 'doubles';
+	requiredDrop = true;
+	requiredEvolution = true;
+	canReroll = true;
+	baseHtmlPageGameName = name;
+	htmlPageGameDescription = description;
+
+	meetsStarterCriteria(pokemon: IPokemon): boolean {
+		return pokemon.evos.length <= 1;
+	}
+
+	meetsEvolutionCriteria(pokemon: IPokemon): boolean {
+		return pokemon.evos.length <= 1;
+	}
+}
+
+export const game: IGameFile<DoublesReleaseAndDevolve> = Games.copyTemplateProperties(eliminationTournamentGame, {
+	aliases: ['doublesrandde', 'doublesrde', 'doublesreleasedevolve', 'drde'],
+	class: DoublesReleaseAndDevolve,
+	description,
+	name,
+	variants: [
+		{
+			name: "Doubles Release and De-volve Ubers",
+			canChangeFormat: false,
+			battleFormatId: "doublesubers",
+			variantAliases: ["ubers", "uber", "doublesubers"],
+		},
+		{
+			name: "Doubles Release and De-volve UU",
+			canChangeFormat: false,
+			battleFormatId: "doublesuu",
+			variantAliases: ["uu", "doublesuu"],
+		},
+	],
+});
---
+++ src/games/doubles-release-and-evolve.ts
@@ -0,0 +1,50 @@
+import type { IGameFile } from '../types/games';
+import type { GameType } from '../types/pokemon-showdown';
+import { game as eliminationTournamentGame } from './templates/battle-elimination';
+import { BattleEliminationTournament } from './templates/battle-elimination-tournament';
+
+const name = "Doubles Release and Evolve";
+const description = "Every player is given a randomly generated team to start out. Each battle that you win, you " +
+	"must 'release' 2 of your Pokemon (remove them from your team) and then evolve 2 Pokemon on your team.";
+
+class DoublesReleaseAndEvolve extends BattleEliminationTournament {
+	canChangeFormat = true;
+	firstRoundExtraTime = 5 * 60 * 1000;
+	activityWarnTimeout: number = 5 * 60 * 1000;
+	autoDqMinutes: number = 5;
+	dropsPerRound = 2;
+	evolutionsPerRound = 2;
+	startingTeamsLength = 6;
+	maxPlayers = 64;
+	minTeamSize = 2;
+	battleFormatId = 'doublesou';
+	battleFormatType: GameType = 'doubles';
+	requiredDrop = true;
+	requiredEvolution = true;
+	canReroll = true;
+	baseHtmlPageGameName = name;
+	htmlPageGameDescription = description;
+	banlist = ['Burmy', 'Caterpie', 'Combee', 'Kricketot', 'Magikarp', 'Scatterbug', 'Sunkern', 'Tynamo', 'Type: Null', 'Weedle',
+		'Wurmple', 'Cosmog', 'Blipbug', 'Snom', 'Wynaut'];
+}
+
+export const game: IGameFile<DoublesReleaseAndEvolve> = Games.copyTemplateProperties(eliminationTournamentGame, {
+	aliases: ['doublesrande', 'doublesre', 'doublesreleaseevolve', 'dre'],
+	class: DoublesReleaseAndEvolve,
+	description,
+	name,
+	variants: [
+		{
+			name: "Doubles Release and Evolve Ubers",
+			canChangeFormat: false,
+			battleFormatId: "doublesubers",
+			variantAliases: ["ubers", "uber", "doublesubers"],
+		},
+		{
+			name: "Doubles Release and Evolve UU",
+			canChangeFormat: false,
+			battleFormatId: "doublesuu",
+			variantAliases: ["uu", "doublesuu"],
+		},
+	],
+});
