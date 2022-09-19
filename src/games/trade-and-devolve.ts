9becf051dc2dc0975c84711f179b9f024775e4bf
Quinton Lee <quivistis@gmail.com>
2022-09-18 22:38:22 -0400

    Add Trade and D/Evolve

---
+++ src/games/trade-and-devolve.ts
@@ -0,0 +1,67 @@
+import type { IGameFile } from '../types/games';
+import type { IPokemon } from '../types/pokemon-showdown';
+import { game as eliminationTournamentGame } from './templates/battle-elimination';
+import { BattleEliminationTournament } from './templates/battle-elimination-tournament';
+
+const name = "Trade and De-volve";
+const description = "Every player is given a randomly generated team to start out. Each battle that you win, you " +
+	"must 'release' 1 of your Pokemon (remove it from your team), 'catch' 1 of your opponent's Pokemon (add it to your team), and then " +
+	"de-volve 1 Pokemon on your team.";
+
+class TradeAndDevolve extends BattleEliminationTournament {
+	banlist = ['Wobbuffet'];
+	canChangeFormat = true;
+	additionsPerRound = 1;
+	dropsPerRound = 1;
+	evolutionsPerRound = -1;
+	startingTeamsLength = 6;
+	maxPlayers = 64;
+	requiredAddition = true;
+	requiredDrop = true;
+	requiredEvolution = true;
+	canReroll = true;
+	firstRoundExtraTime = 5 * 60 * 1000;
+	baseHtmlPageGameName = name;
+	htmlPageGameDescription = description;
+
+	meetsStarterCriteria(pokemon: IPokemon): boolean {
+		return pokemon.evos.length <= 1;
+	}
+
+	meetsEvolutionCriteria(pokemon: IPokemon): boolean {
+		return pokemon.evos.length <= 1;
+	}
+}
+
+export const game: IGameFile<TradeAndDevolve> = Games.copyTemplateProperties(eliminationTournamentGame, {
+	aliases: ['tandde', 'tde', 'tradedevolve'],
+	class: TradeAndDevolve,
+	description,
+	name,
+	variants: [
+		{
+			name: "Trade and De-volve Ubers",
+			canChangeFormat: false,
+			battleFormatId: "ubers",
+			variantAliases: ["ubers", "uber"],
+		},
+		{
+			name: "Trade and De-volve UU",
+			canChangeFormat: false,
+			battleFormatId: "uu",
+			variantAliases: ["uu"],
+		},
+		{
+			name: "Trade and De-volve RU",
+			canChangeFormat: false,
+			battleFormatId: "ru",
+			variantAliases: ["ru"],
+		},
+		{
+			name: "Trade and De-volve NU",
+			canChangeFormat: false,
+			battleFormatId: "nu",
+			variantAliases: ["nu"],
+		},
+	],
+});
---
+++ src/games/trade-and-evolve.ts
@@ -0,0 +1,66 @@
+import type { IGameFile } from '../types/games';
+import type { IPokemon } from '../types/pokemon-showdown';
+import { game as eliminationTournamentGame } from './templates/battle-elimination';
+import { BattleEliminationTournament } from './templates/battle-elimination-tournament';
+
+const name = "Trade and Evolve";
+const description = "Every player is given a randomly generated team to start out. Each battle that you win, you " +
+	"must 'release' 1 of your Pokemon (remove it from your team), 'catch' 1 of your opponent's Pokemon (add it to your team), and then " +
+	"evolve 1 Pokemon on your team.";
+const bannedTiers: string[] = ['Uber', 'OU', 'UU', 'UUBL', 'RU', 'RUBL', 'NU', 'NUBL', 'PU', 'PUBL'];
+
+class TradeAndEvolve extends BattleEliminationTournament {
+	canChangeFormat = true;
+	additionsPerRound = 1;
+	dropsPerRound = 1;
+	evolutionsPerRound = 1;
+	startingTeamsLength = 6;
+	maxPlayers = 64;
+	requiredAddition = true;
+	requiredDrop = true;
+	requiredEvolution = true;
+	canReroll = true;
+	firstRoundExtraTime = 5 * 60 * 1000;
+	baseHtmlPageGameName = name;
+	htmlPageGameDescription = description;
+	banlist = ['Burmy', 'Caterpie', 'Combee', 'Kricketot', 'Magikarp', 'Scatterbug', 'Sunkern', 'Tynamo', 'Type: Null', 'Weedle',
+		'Wurmple', 'Cosmog', 'Blipbug', 'Snom', 'Wynaut'];
+
+	meetsStarterCriteria(pokemon: IPokemon): boolean {
+		if (bannedTiers.includes(pokemon.tier)) return false;
+		return true;
+	}
+}
+
+export const game: IGameFile<TradeAndEvolve> = Games.copyTemplateProperties(eliminationTournamentGame, {
+	aliases: ['tande', 'te', 'tradeevolve'],
+	class: TradeAndEvolve,
+	description,
+	name,
+	variants: [
+		{
+			name: "Trade and Evolve Ubers",
+			canChangeFormat: false,
+			battleFormatId: "ubers",
+			variantAliases: ["ubers", "uber"],
+		},
+		{
+			name: "Trade and Evolve UU",
+			canChangeFormat: false,
+			battleFormatId: "uu",
+			variantAliases: ["uu"],
+		},
+		{
+			name: "Trade and Evolve RU",
+			canChangeFormat: false,
+			battleFormatId: "ru",
+			variantAliases: ["ru"],
+		},
+		{
+			name: "Trade and Evolve NU",
+			canChangeFormat: false,
+			battleFormatId: "nu",
+			variantAliases: ["nu"],
+		},
+	],
+});
