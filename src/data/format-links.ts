05e1f15471298c38bbb067d3ade987794cccf694
Quinton Lee <quivistis@gmail.com>
2022-12-12 00:21:18 -0600

    Update pokemon-showdown-sha

--- src/data/format-links.ts
+++ src/data/format-links.ts
@@ -114,9 +114,9 @@ export const formatLinks: Dict<IFormatDataLinks> = {
 	// 'gen8revelationmons': {
 	// 	teams: 'https://www.smogon.com/forums/threads/3692297/#post-9005095',
 	// },
-	'gen8inheritance': {
-		teams: 'https://www.smogon.com/forums/threads/3656811/#post-8292871',
-	},
+	// 'gen8inheritance': {
+	// 	teams: 'https://www.smogon.com/forums/threads/3656811/#post-8292871',
+	// },
 	'gen8godlygift': {
 		teams: 'https://www.smogon.com/forums/threads/3660461/#post-8376501',
 	},
