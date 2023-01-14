502ea1c3d300e331f98ab0d99189321a8f3ea3f2
Quinton Lee <quivistis@gmail.com>
2023-01-14 01:43:53 -0600

    Data: fix typo

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -205,7 +205,7 @@ export const categories: CategoryData = {
 	Sunflora: 'Sun',
 	Yanma: 'Clear Wing',
 	Wooper: 'Water Fish',
-	Wooper-Paldea: 'Poison Fish',
+	'Wooper-Paldea': 'Poison Fish',
 	Quagsire: 'Water Fish',
 	Espeon: 'Sun',
 	Umbreon: 'Moonlight',
